3e674dbb840d962142a4bf43b0f7700bc42e2126
Anastasiia Tokareva <wwwnasty11@rambler.ru>
2024-06-20 17:05:31 +0300

    your commit

---
+++ src/main/java/ru/netology/services/cycles/vacation/services/VacationService.java
@@ -0,0 +1,19 @@
+package ru.netology.services.cycles.vacation.services;
+
+public class VacationService {
+    public int calcVacationMonths(int income, int expenses, int threshold) {
+        int money = 0;
+        int vacationMonths = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money < threshold){
+                money += income;// money = money + income
+                money -= expenses;
+            } else{
+                money -= expenses;
+                money=money/3;
+                vacationMonths++;
+            }
+        }
+        return vacationMonths;
+    }
+}
---
+++ src/test/java/ru/netology/services/cycles/vacation/services/VacationServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.services.cycles.vacation.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VacationServiceTest {
+
+    @Test
+    public void testCashVacation(){
+        VacationService service = new VacationService();
+
+        int expected = 3;
+
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+
+        int actual = service.calcVacationMonths(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testCashVacationRest(){
+
+        VacationService service = new VacationService();
+
+        int expected = 2;
+
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+
+        int actual = service.calcVacationMonths(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
+
+
